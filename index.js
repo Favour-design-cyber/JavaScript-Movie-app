11f7ffad1fce603700602627d5001f475d052139
Favour-design-cyber <oluwadamilarefavour2000@gmail.com>
2023-03-22 14:01:19 +0100

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
-      apikey: "e2d4ef15",
-      s: "avengers",
-    }
+      apikey: "d9835cc5",
+      s: searchTerm,
+    },
   });
 
-  console.log(response.data);
-};
+  if (response.data.Error) {
+    return [];
+  }
 
-fetchData();
+  return response.data.Search;
+};
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a movie</b></label>
+<input class="input"/>
+<div class="dropdown">
+<div class="dropdown-menu">
+<div class="dropdown-content results"></div>
+</div>
+</div>
+`;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.title}
+    `;
+
+    resultsWrapper.appendChild(option);
+  }
+};
+input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
